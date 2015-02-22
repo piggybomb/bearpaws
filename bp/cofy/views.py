35a264e627c6ddb0efa3cbc647985d5d61908794
Ubuntu <ubuntu@ip-172-31-36-133.us-west-2.compute.internal>
2015-02-22 09:15:20 +0000

    implmented joblist page and initilized job page

--- bp/cofy/views.py
+++ bp/cofy/views.py
@@ -9,11 +9,12 @@ from cofy.models import employment, employment_growth, immigration, job_openings
 def index(request):
 	return render_to_response('cofy/index.html', context_instance=RequestContext(request))
 
-def job(request, job_noc, slug):
-    job = cofy_occupational_groupings.objects.get(noc = job_noc)
-    
+def job(request, job_noc):
+    job = occupational_groupings.objects.get(noc = job_noc)
     result = render_to_response('cofy/job.html', {"job", job})
     return result
 
 def joblist(request):
-	return render_to_response('cofy/joblist.html', context_instance=RequestContext(request))
+	jobs = occupational_groupings.objects.all()
+	results = render_to_response('cofy/joblist.html', {'jobs':jobs}, context_instance = RequestContext(request))
+	return results

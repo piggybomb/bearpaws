33de6f2a185a10b5113482209531e5c0a6d84e6b
jlee91 <jjl29@sfu.ca>
2015-02-21 18:09:03 -0800

    added bootstrap files, changed homepage from default django to index.html and a placeholder base.html template

--- bp/bp/urls.py
+++ bp/bp/urls.py
@@ -1,3 +1,4 @@
+from cofy import views
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -7,4 +8,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'cofy.views.index'),
 )
